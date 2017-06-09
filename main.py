c2e71470e3cd99493a81c367f6467b6b8fba7087
reinvantveer <rein.van.t.veer@geodan.nl>
2017-06-09 13:11:02 +0200

    Add convolutional layer

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+# LSTM and CNN for sequence classification in the IMDB dataset
+import time
+start_time = time.time()
+
 import numpy
 from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
-
-import time
-start_time = time.time()
 
 # fix random seed for reproducibility
 numpy.random.seed(7)
@@ -25,14 +28,15 @@ X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
 embedding_vector_length = 32
 model = Sequential()
 model.add(Embedding(top_words, embedding_vector_length, input_length=max_review_length))
+model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+model.add(MaxPooling1D(pool_size=2))
 model.add(LSTM(100))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=64)
+model.fit(X_train, y_train, epochs=3, batch_size=64)
 
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
-
 print("--- %s seconds ---" % (time.time() - start_time))
